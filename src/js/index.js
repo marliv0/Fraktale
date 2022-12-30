dfc2c50ce827a09fd67af9b90dea6fb34442fce0
Marko Livajusic <macakacc4gota@gmail.com>
2022-12-30 23:51:35 +0100

    Change iframe size

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,19 @@ function a() {
     console.log("clicked...");
     // document.querySelector(".introduction").scrollIntoView();
 }
+
+
+console.log("pain...");
+const text = document.getElementById("pls");
+
+const callback = (entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            text.style = "visibility: visible; animation: fadeIn ease 3s;";
+        }
+    });
+};
+
+// Create the IntersectionObserver
+const observer = new IntersectionObserver(callback, {rootMargin: "10000px 0px 10000px 0px"});
+observer.observe(text);
