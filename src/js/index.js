0e7499ccb0b49f8042f5b0121ac3e50f31f23360
Marko Livajusic <macakacc4gota@gmail.com>
2023-01-02 17:21:19 +0100

    Working sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,30 @@
 function a() {
     console.log("clicked...");
-    // document.querySelector(".introduction").scrollIntoView();
+    document.querySelector(".articles").scrollIntoView();
+}
+
+function scrollTo(id) {
+    // document.querySelector(`.${id}`).scrollIntoView();
+    console.log("scroll to: ", id);
 }
 
 function fillSidebar() {
     const topics = document.getElementsByClassName("topic");
-    // let topic;
+    let topic;
 
     for (let i = 0; i < topics.length; i++) {
         const element = document.createElement("li");
-        element.innerText = topics[i].innerText;
-        // element.onclick = document.querySelector()
+        topic = topics[i].innerText;
+
+        element.innerText = topic;
+        element.onclick = function () {
+                document.getElementById(element.innerText).scrollIntoView();
+        };
         document.getElementById("toc").appendChild(element);
     }
 }
 
 /*
-console.log("pain...");
 const text = document.getElementById("pls");
 
 const callback = (entries) => {
