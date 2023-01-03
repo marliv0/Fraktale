d8cc4b3c4587192c7d87f6f9fc982176e9bdde5b
Marko Livajusic <macakacc4gota@gmail.com>
2023-01-03 18:04:50 +0100

    Sidebar: hover colors, paddings

--- src/js/index.js
+++ src/js/index.js
@@ -4,8 +4,9 @@ function scrollToArticles() {
 
 function fillSidebar() {
     // let topic;
-    
     const topics = document.getElementsByClassName("topic");
+    let j = 1;
+
     console.log(topics);
     for (let i = 1; i < topics.length + 1; i++) {
         const element = document.createElement("li");
@@ -13,24 +14,26 @@ function fillSidebar() {
 
 
         element.innerText = `${i}. ${topic}`;
+        element.style="margin: 10px 0;";
+        element.onmouseover = function() {
+            element.style.cssText += "color: white;";
+        }
+        element.onmouseleave = function() {
+            element.style.cssText += "color: #818181";
+        }
+
         element.onclick = function () {
             document.getElementById(topic).scrollIntoView();
         };
-
+        
         switch (topics[i].nodeName) {
-            /*
-            case "H1":
-                element.style = "font-size: 24px;";
-                break;
-            case "H2":
-                element.style = "font-size: 22px;";
-                break;
-                */
             case "H3":
-                element.style = "padding-left: 25px;font-size: 18px;";
-                element.innerText = `${topic}`;
+                element.style = "padding-left: 10px;font-size: 18px; margin: 5px;";
+                element.innerText = `${j}. ${topic}`;
+                j++;
                 break;
         }
+
         document.getElementById("sidebar").appendChild(element);
     }
 }
