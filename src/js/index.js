e4733e2660ab8cc0506971eb56aea21d6b0b50a0
Marko Livajusic <macakacc4gota@gmail.com>
2023-01-04 12:07:40 +0100

    Theory: finish contents

--- src/js/index.js
+++ src/js/index.js
@@ -3,37 +3,32 @@ function scrollToArticles() {
 }
 
 function fillSidebar() {
-    // let topic;
     const topics = document.getElementsByClassName("topic");
-    let j = 1;
-
-    console.log(topics);
     for (let i = 1; i < topics.length + 1; i++) {
         const element = document.createElement("li");
         let topic = topics[i].innerText;
+        let id = topics[i].id;
 
-
-        element.innerText = `${i}. ${topic}`;
+        element.innerText = `${topic}`;
         element.style="margin: 10px 0;";
+        
         element.onmouseover = function() {
             element.style.cssText += "color: white;";
         }
+
         element.onmouseleave = function() {
             element.style.cssText += "color: #818181";
         }
 
         element.onclick = function () {
-            document.getElementById(topic).scrollIntoView();
+            document.getElementById(id).scrollIntoView();
         };
         
         switch (topics[i].nodeName) {
             case "H3":
                 element.style = "padding-left: 10px;font-size: 18px; margin: 5px;";
-                element.innerText = `${j}. ${topic}`;
-                j++;
                 break;
         }
-
         document.getElementById("sidebar").appendChild(element);
     }
 }
