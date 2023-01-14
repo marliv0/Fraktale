2b79fae1dd08a1d46f8089b6fcba1d2044a0c2d9
Marko Livajusic <marko_livajusic0@protonmail.com>
2023-01-14 22:50:18 +0100

    Formalities

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,3 +1,15 @@
+/**
+ * Funktionalität zur zufälligen Bildgenerierung mithilfe von Pexels-API.
+ * 
+ * 2023 Marko Livajusic (marko_livajusic0 <at> protonmail.com) 
+ */
+
+/**
+ * Funktion, welche die URLs durchgeht und in einem Array speichert.
+ * 
+ * @param {*} images 
+ * @returns Array von URLs (der Bilder).
+ */
 function getImgURLS(images) {
     let urls = []
     images.map(image => {
@@ -7,6 +19,11 @@ function getImgURLS(images) {
     return urls;
 }
 
+/**
+ * Erstellt die Bilder (img) in HTML und fügt sie in das Body ein.
+ *  
+ * @param {*} urls Array von URLs der Bilder. 
+ */
 function createImgElements(urls) {
     let image;
     for (let i = 0; i < urls.length; i++) {
@@ -20,11 +37,17 @@ function createImgElements(urls) {
 }
 
 const KEYWORD = "fractal";
+
+/*
+ * Anzahl der Bilder pro Seite sowie die Seitennummer,
+ * von der die Bilder gezogen werden, sollen einigermaßen zufällig sein.
+*/ 
 const AMOUNT = Math.floor(Math.random() * 35) + 10
 const PAGE = Math.floor(Math.random() * 4) + 1
 
 fetch(`https://api.pexels.com/v1/search?query=${KEYWORD}&per_page=${AMOUNT}&page=${PAGE}`, {
         headers: {
+            // API-Schlüssel. Sollte lieber nicht öffentlich sein!
             Authorization: "563492ad6f9170000100000102b516d3cf6245c1a66e3a166bd573b3",
         }
     })
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
 /**
- * Funktion fürs Scrollen bis zum div "articles"
+ * Funktionalitäten zum Ausfüllen der Seitenleiste (theory.html) und
+ * zur Überprüfung, ob sich HTML-Elemente auf dem Bildschirm befinden,
+ * um Animationen rechtzeitig abspielen zu können (index.html).
+ * 
+ * 2023 Marko Livajusic (marko_livajusic0 <at> protonmail.com) 
+ */
+
+/**
+ * Funktion für das Scrollen bis zum div "articles"
  */
 function scrollToArticles() {
     document.querySelector(".articles").scrollIntoView();
@@ -47,13 +55,18 @@ function fillSidebar() {
     }
 }
 
-
+/** 
+ * Callback-Funktion für die HTML-Elemente.
+ * @param {*} entries Array von HTML-Elementen, bei denen überprüft werden soll, ob sie sich im Bildschirmbereich befinden. 
+ */
 const callback = (entries) => {
     entries.forEach(
         (entry) => {
             if (entry.isIntersecting) {
+                // Wenn im Bildschirmbereich, soll die Animation abgespielt werden.
                 entry.target.style.cssText += "animation: fadeIn 2s;";
             } else {
+                // Wenn nicht im Bildschirmbereich, dann soll auch keine Animation abgespielt werden.
                 entry.target.style.animation = "none";
             }
         }
@@ -62,6 +75,8 @@ const callback = (entries) => {
 }
 
 let observer = new IntersectionObserver(callback);
+
+// Elemente, deren Position auf dem Bildschirm wir überprüfen sollen.
 let elements = [document.getElementById("introduction"), document.getElementById("articles")];
 
 for (let i = 0; i < elements.length; i++) {
